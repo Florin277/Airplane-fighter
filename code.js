326f67b6e65eedf23b39c092a705e572d04c8deb
Florin277 <simoncaflorin79@gmail.com>
2025-08-06 18:03:09 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,84 +1,95 @@
+const ONE_SECOND_MS = 1000;
+const SPEED_MS = 30;
+const MOVE_AIRPLANE = 15;
+const SIZE_IMAGE = 30;
+const LEFT_ARROW = 37;
+const REIGHT_ARROW = 39;
 const  gameBoard= document.getElementById("playArea"); 
 const  contex = gameBoard.getContext("2d");
-const sizeImage = 30;
-const leftArrow = 37;
-const reightArrow = 39;
 const object = [];
 const imageSource = ["Images/airplane2.png", "Images/ball.png", "Images/parachute jumper1.png", "Images/parachute jumper2.png", "Images/stork.png"];
 const images = [new Image(), new Image(), new Image(), new Image(), new Image()];
 const airplaneImage = new Image();
 let xAirplanePos = gameBoard.width / 2;
-let yAirplanePos = gameBoard.height - sizeImage;
+let yAirplanePos = gameBoard.height - SIZE_IMAGE;
 let updateGameBoard;
 let updateObject;
 let airplaneSpeed;
+let speedAirplane = 100;
+let speedObject = 1;
+let speedGame = 15;
 let seconds = 0;
 airplaneImage.src = "Images/airplane1.png";
 
-setInterval(function(){++seconds},1000); 
+setInterval(function(){++seconds},ONE_SECOND_MS); 
 
 for (let i = 0; i < 5; ++i) {
   images[i].src = imageSource[i];
 }
 
-airplaneSpeed = setInterval( () => {
-  contex.fillStyle = "black";
-  for (let i = 15; i > 0; --i) {
-    let x = Math.random() * contex.canvas.width;
-    let y = Math.random() * contex.canvas.height;
-    contex.fillRect(x, y, 1, 3);
-  }
-}, 50)
-
 function pushObject() {
   const imageIndex = Math.floor(Math.random() * 5);
   object.push({
-    xObjectPos: Math.random() * (gameBoard.width - sizeImage),
+    xObjectPos: Math.random() * (gameBoard.width- SIZE_IMAGE),
     yObjectPos: 0,
-    speedObject: 5,
-    sizeObject: 30,
+    sizeObject: SIZE_IMAGE,
+    speed: speedObject,
     img: images[imageIndex],
   })
 }
+pushObject();
+
+updateObject = setInterval(() => {
+  for (let i = 0; i < object.length; ++i) {
+    object[i].yObjectPos += object[i].speed;
+  }
+  if (object[object.length - 1].yObjectPos > SIZE_IMAGE) {
+    pushObject();
+  }
+}, SPEED_MS)
+
+airplaneSpeed = setInterval( () => {
+  contex.fillStyle = "black";
+  for (let i = speedAirplane; i > 0; --i) {
+    let x = Math.random() * contex.canvas.width;
+    let y = Math.random() * contex.canvas.height;
+    contex.fillRect(x, y, 1, 5);
+  }
+}, SPEED_MS)
 
 updateGameBoard = setInterval(() => {
   contex.clearRect(0, 0, gameBoard.width, gameBoard.height);
-  contex.drawImage(airplaneImage, xAirplanePos, yAirplanePos, sizeImage, sizeImage);
-  pushObject();
-  for (let obj of object ) {
+  contex.drawImage(airplaneImage, xAirplanePos, yAirplanePos, SIZE_IMAGE, SIZE_IMAGE);
+  for (let obj of object) {
     contex.drawImage(obj.img, obj.xObjectPos, obj.yObjectPos, obj.sizeObject, obj.sizeObject);
     collisionCheck(xAirplanePos, yAirplanePos, obj.xObjectPos, obj.yObjectPos);
   }
-}, 750)
+},SPEED_MS * 2);
 
 function collisionCheck(planeX, planeY, objectX, objectY) {
-  if (planeX < objectX + sizeImage && planeX + sizeImage > objectX &&
-    planeY < objectY + sizeImage && planeY + sizeImage > objectY) {
+  if (planeX < objectX + SIZE_IMAGE && planeX + SIZE_IMAGE > objectX &&
+    planeY < objectY + SIZE_IMAGE && planeY + SIZE_IMAGE > objectY) {
     let secondsString = seconds.toString();
     contex.clearRect(0, 0, gameBoard.height, gameBoard.width);
-    contex.font = "350px serif";
-    contex.fillText(secondsString, 50, 330);
+    object.length = 0;
     clearInterval(updateObject);
     clearInterval(updateGameBoard);
     clearInterval(airplaneSpeed);
+    document.getElementById('score').innerHTML = "Score: " + secondsString;
   }
 }
 
 function moveAirplane(pressArrow) {
-  if (pressArrow.keyCode === leftArrow && xAirplanePos > 0) {
-    xAirplanePos -= 15;
-  } else if (pressArrow.keyCode === reightArrow && 
-    xAirplanePos < gameBoard.width - sizeImage - 1 ) {
-      xAirplanePos += 15;
-    }
+  if (pressArrow.keyCode === LEFT_ARROW && xAirplanePos > 0) {
+    xAirplanePos -= MOVE_AIRPLANE;
+  } else if (pressArrow.keyCode === REIGHT_ARROW && 
+    xAirplanePos < gameBoard.width - SIZE_IMAGE - 1 ) {
+    xAirplanePos += MOVE_AIRPLANE;
   }
+}
 document.onkeydown = moveAirplane;
 
-updateObject = setInterval(() => {
-  for (let obj of object) {
-    obj.yObjectPos += obj.speedObject;
-  }
-}, 75)
+
 
 
 
